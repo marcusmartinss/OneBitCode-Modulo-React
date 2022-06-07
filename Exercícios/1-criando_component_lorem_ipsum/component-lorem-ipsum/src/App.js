66b32c7454b001809d0283f700b1fcc7a46d3a06
Marcus <vini.mark2002@gmail.com>
2022-06-06 23:49:09 -0300

    Exercício Criando um component Lorem Ipsum

---
+++ Exercícios/1-criando_component_lorem_ipsum/component-lorem-ipsum/src/App.js
@@ -0,0 +1,13 @@
+import LoremIpsum from './components/Lorem Ipsum'
+import HelloWorld from './components/Hello';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+      <LoremIpsum />
+    </div>
+  );
+}
+
+export default App;
