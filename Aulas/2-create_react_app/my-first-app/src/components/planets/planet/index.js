391c6695bbb9ee1b7896f671a0b83f86f74ccf3a
Marcus <vini.mark2002@gmail.com>
2022-06-08 21:02:23 -0300

    Aula Dividindo e reutilizando components

--- Aulas/2-create_react_app/my-first-app/src/components/planets/planet/index.js
+++ Aulas/2-create_react_app/my-first-app/src/components/planets/planet/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import  './style.css'
+import GrayImg from '../../shared/gray-img';
 
 const Planet = () => {
     return (
         <div>
             <h4>Mercúrio</h4>
-            <p>
-                Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar.
-            </p>
-            <img class="gray-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/800px-Mercury_in_color_-_Prockter07-edit1.jpg"></img>
+            <p>Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar.</p>
+            <GrayImg />
         </div>
     )
 }
---
+++ Aulas/2-create_react_app/my-first-app/src/components/shared/gray-img/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import  './style.css'
+
+const GrayImg = () => {
+    return(
+        <img class="gray-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/800px-Mercury_in_color_-_Prockter07-edit1.jpg"></img>
+    )
+}
+
+export default GrayImg;
