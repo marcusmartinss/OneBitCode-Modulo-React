93b6e0238eb3d0f2701883eea0db22993e6e1d50
Marcus <vini.mark2002@gmail.com>
2022-06-15 23:16:28 -0300

    Aula Incluindo Lifecycle na nossa classe
    - Foram utilizados arquivos json simulando uma api
    - Foi implementado o Lifecycle

--- Aulas/2-create_react_app/my-first-app/src/components/planets/index.js
+++ Aulas/2-create_react_app/my-first-app/src/components/planets/index.js
@@ -1,26 +1,29 @@
 import React, { Fragment } from "react";
 import Planet from "./planet";
 
+async function getPlanets() {
+    let response = await fetch(
+        'http://localhost:3000/api/planets.json'
+    )
+    let data = await response.json()
+    return data;
+}
+
 class Planets extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            planets: [
-                {
-                    name: "Marte",
-                    description_text: 'Marte é o quarto planeta a partir do Sol, o segundo menor do Sistema Solar. Batizado em homenagem ao deus romano da guerra, muitas vezes é descrito como o "Planeta Vermelho", porque o óxido de ferro predominante em sua superfície lhe dá uma aparência avermelhada.',
-                    description_url: "https://pt.wikipedia.org/wiki/Marte_(planeta)",
-                    img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/280px-OSIRIS_Mars_true_color.jpg",
-                },
-                {
-                    name: "Saturno",
-                    description_text: "Saturno é o sexto planeta a partir do Sol e o segundo maior do Sistema Solar atrás de Júpiter. Pertencente ao grupo dos gigantes gasosos, possui cerca de 95 massas terrestres e orbita a uma distância média de 9,5 unidades astronômicas.",
-                    description_url: 'https://pt.wikipedia.org/wiki/Saturno_(planeta)',
-                    img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Saturn_during_Equinox.jpg/300px-Saturn_during_Equinox.jpg"
-                },
-            ]
+            planets: [ ]
         }
     };
+
+    componentDidMount() {
+        getPlanets().then(data => {
+            this.setState(state => ({
+                planets: data['planets']
+            }))
+        })
+    }
 
     removeLastPlanet = () => {
         let new_planets = [...this.state.planets]
